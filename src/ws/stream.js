61b1209aca0112fb4d3843bddd119dd65dbe7e44
Akshay Rajan <akshayrajan345@gmail.com>
2025-05-05 07:26:39 +0530

    Fix typo

--- src/ws/stream.js
+++ src/ws/stream.js
@@ -10,24 +10,16 @@ const stream = ( socket ) => {
         }
     } );
 
-
     socket.on( 'newUserStart', ( data ) => {
         socket.to( data.to ).emit( 'newUserStart', { sender: data.sender } );
     } );
-
 
     socket.on( 'sdp', ( data ) => {
         socket.to( data.to ).emit( 'sdp', { description: data.description, sender: data.sender } );
     } );
 
-
     socket.on( 'ice candidates', ( data ) => {
         socket.to( data.to ).emit( 'ice candidates', { candidate: data.candidate, sender: data.sender } );
-    } );
-
-
-    socket.on( 'chat', ( data ) => {
-        socket.to( data.room ).emit( 'chat', { sender: data.sender, msg: data.msg } );
     } );
 };
 
